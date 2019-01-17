3658817b22f7bf8ac9eaf598e365b7cafdd42411
Tom Flenniken <tom.flenniken@slalom.com>
2019-01-17 13:05:02 -0800

    Added controller and request/response scaffolding.

---
+++ src/main/java/com/google/cloudy/retention/controller/EventsController.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2019 Google LLC. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the “License”);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an “AS IS” BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and limitations under the License.
+ *
+ * Any software provided by Google hereunder is distributed “AS IS”,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, and is not intended for production use.
+ */
+
+package com.google.cloudy.retention.controller;
+
+import com.google.cloudy.retention.controller.pojo.request.EventExecutionRequest;
+import com.google.cloudy.retention.controller.pojo.response.EventExecutionResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/** Controller for exposing event based behavior */
+@Path("/events")
+public class EventsController extends BaseController {
+
+  private static final Logger logger = LoggerFactory.getLogger(EventsController.class);
+
+  /** Accepts a request to invoke a policy or process a delete marker */
+  @POST
+  @Path("/execution")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response executeEvent(EventExecutionRequest request) {
+    String requestUuid = generateRequestUuid();
+
+    try {
+      validateEventExecution(request);
+
+      // TODO: Perform business logic
+
+      EventExecutionResponse response = new EventExecutionResponse();
+      response.setRequestUuid(requestUuid);
+
+      return Response.status(200).entity(response).build();
+    } catch (HttpException exception) {
+      return generateExceptionResponse(exception, requestUuid);
+    }
+  }
+
+  private void validateEventExecution(EventExecutionRequest request) throws ValidationException {
+    // TODO
+    throw new NotImplementedException();
+  }
+}
